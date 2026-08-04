f6a412cec5a00a94f73385a4410d56cf332f4f69
autofix-eval <eval@humanbehavior.local>
2026-08-04 16:10:47 -0700

    chore(eval): plant O38 promise-finally evalmsf9ufb1

---
+++ src/lib/eval_o38_evalmsf9ufb1.ts
@@ -0,0 +1,9 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsf9ufb1 */
+export async function evalO38_evalmsf9ufb1() {
+  try {
+    await Promise.reject(new TypeError("boom"));
+  } finally {
+    return "ok";
+  }
+}
