7b7902742da41ac4d18a2f4b74129945e7df06fa
autofix-eval <eval@humanbehavior.local>
2026-08-04 16:10:39 -0700

    chore(eval): plant O36 generator evalmsf9ua3e

---
+++ src/lib/eval_o36_evalmsf9ua3e.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsf9ua3e */
+export function evalO36_evalmsf9ua3e() {
+  function* g() { if (false) yield 1; }
+  return g().next().value!.toFixed(0);
+}
