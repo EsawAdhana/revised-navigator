cbfc057efdc0d7a33657ed70b2ac1e2d21a8edca
autofix-eval <eval@humanbehavior.local>
2026-08-04 14:11:08 -0700

    chore(eval): plant PNest evalmsf5kkxm

---
+++ src/lib/eval_pnest_evalmsf5kkxm.ts
@@ -0,0 +1,4 @@
+/** Autofix eval plant PNest evalmsf5kkxm */
+export function evalPNest_evalmsf5kkxm(input: { user?: { profile?: { name?: string } } }) {
+  return input.user!.profile!.name!.toUpperCase();
+}
