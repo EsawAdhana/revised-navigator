4683d7452941e99ffbdad7e3ecafa5395509446a
autofix-eval <eval@humanbehavior.local>
2026-08-04 16:06:24 -0700

    chore(eval): plant O26 reverse-mutate evalmsf9ort3

---
+++ src/lib/eval_o26_evalmsf9ort3.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsf9ort3 */
+export function evalO26_evalmsf9ort3(items: string[]) {
+  const head = items[0];
+  items.reverse();
+  return head!.concat(items[0]!);
+}
