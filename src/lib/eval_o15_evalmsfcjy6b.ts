3e35d4d1bf8796034cd352d442d536f15e0662c9
autofix-eval <eval@humanbehavior.local>
2026-08-04 17:26:35 -0700

    chore(eval): plant O15 for-in evalmsfcjy6b

---
+++ src/lib/eval_o15_evalmsfcjy6b.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsfcjy6b */
+export function evalO15_evalmsfcjy6b(obj: Record<string, number>) {
+  const keys: string[] = [];
+  for (const k in obj) keys.push(k);
+  return keys[0]!.toUpperCase();
+}
