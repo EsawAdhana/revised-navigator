4db01b43ba286e6e58cf31f0ab0eff2bc62dd442
autofix-eval <eval@humanbehavior.local>
2026-08-04 20:37:24 -0700

    chore(eval): plant O26 reverse-mutate evalmsfjdcez

---
+++ src/lib/eval_o26_evalmsfjdcez.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsfjdcez */
+export function evalO26_evalmsfjdcez(items: string[]) {
+  const head = items[0];
+  items.reverse();
+  return head!.concat(items[0]!);
+}
