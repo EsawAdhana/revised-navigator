719d774859d957a5706fe2091c89ce6d89566031
autofix-eval <eval@humanbehavior.local>
2026-08-04 17:26:38 -0700

    chore(eval): plant O16 switch-fallthrough evalmsfcjz5f

---
+++ src/lib/eval_o16_evalmsfcjz5f.ts
@@ -0,0 +1,15 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsfcjz5f */
+export function evalO16_evalmsfcjz5f(code: number) {
+  let label: string;
+  switch (code) {
+    case 1:
+      label = "one";
+    case 2:
+      label = "two";
+      break;
+    default:
+      label = "other";
+  }
+  return label.toUpperCase();
+}
