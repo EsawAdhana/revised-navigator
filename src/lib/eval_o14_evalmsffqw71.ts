da11c2003d4db001388415edae3aba5a265962e0
autofix-eval <eval@humanbehavior.local>
2026-08-04 18:55:58 -0700

    chore(eval): plant O14 sparse-array evalmsffqw71

---
+++ src/lib/eval_o14_evalmsffqw71.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsffqw71 */
+export function evalO14_evalmsffqw71() {
+  const a: Array<number | undefined> = [];
+  a[2] = 1;
+  return a.map((x) => x!.toFixed(0))[0]!;
+}
