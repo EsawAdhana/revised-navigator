dd463dc7c17b1911be0ee5f352153eb6dc66d3b2
autofix-eval <eval@humanbehavior.local>
2026-08-04 21:12:22 -0700

    chore(eval): plant O38 promise-finally evalmsfkmavn

---
+++ src/lib/eval_o38_evalmsfkmavn.ts
@@ -0,0 +1,9 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsfkmavn */
+export async function evalO38_evalmsfkmavn() {
+  try {
+    await Promise.reject(new TypeError("boom"));
+  } finally {
+    return "ok";
+  }
+}
