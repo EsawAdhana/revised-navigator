11f065f3df598d167fbf8683267907e8d41958d6
autofix-eval <eval@humanbehavior.local>
2026-08-04 17:27:08 -0700

    chore(eval): plant O27 splice-slice evalmsfckmak

---
+++ src/lib/eval_o27_evalmsfckmak.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsfckmak */
+export function evalO27_evalmsfckmak(items: string[]) {
+  const rest = items.splice(1);
+  return rest[0]!.toUpperCase();
+}
