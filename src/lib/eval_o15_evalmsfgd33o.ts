280a9d1d90c0894ebd2f03796a43b2bbdf66a94d
autofix-eval <eval@humanbehavior.local>
2026-08-04 19:13:14 -0700

    chore(eval): plant O15 for-in evalmsfgd33o

---
+++ src/lib/eval_o15_evalmsfgd33o.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+/** Autofix obscure plant evalmsfgd33o */
+export function evalO15_evalmsfgd33o(obj: Record<string, number>) {
+  const keys: string[] = [];
+  for (const k in obj) keys.push(k);
+  return keys[0]!.toUpperCase();
+}
